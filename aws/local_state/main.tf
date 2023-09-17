97abd61a83029b279f5f5266950fcf8bbb96db25
priyulp <93876688+priyulp@users.noreply.github.com>
2023-09-18 00:07:55 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -11,10 +11,13 @@ terraform {
 
 provider "aws" {
   region  = "us-west-2"
+  AWS_ACCESS_KEY = "AKIA5TQPF5JBOZLMM3WN"
+  AWS_SECRET_ACCESS_KEY = "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"
+
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
 
   tags = {
