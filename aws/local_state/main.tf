6c3b1b20436ee50f5286470d99dcfba88e4e3963
priyulp <93876688+priyulp@users.noreply.github.com>
2023-09-19 13:42:25 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
 
   tags = {
