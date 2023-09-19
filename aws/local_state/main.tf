22d8fd43736a53aa5314b97e39fccab5afeb0fd2
priyulp <93876688+priyulp@users.noreply.github.com>
2023-09-19 14:02:04 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,4 +1,4 @@
-terraform {
+//terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,7 +7,7 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
-}
+}//
 
 provider "aws" {
   region  = "us-west-2"
