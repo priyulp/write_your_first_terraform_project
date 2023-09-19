59f2515b5b75464323557446b45b7ffa6eda2bc7
priyulp <93876688+priyulp@users.noreply.github.com>
2023-09-19 14:17:03 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,22 +1,10 @@
-//terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}//
-
 provider "aws" {
   region  = "us-west-2"
   access_key = "AKIA5TQPF5JBOZLMM3WN"
   secret_key = "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"
-
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_Instance" "project" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
 
